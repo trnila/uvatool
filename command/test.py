ade6f07b02b5a107e0d66c1ddf4d41f02b4444a7
Daniel Trnka <daniel.trnka@gmail.com>
2016-09-16 00:24:10 +0200

    fixed testing

--- command/test.py
+++ command/test.py
@@ -16,11 +16,19 @@ class Test():
         os.system(subprocess.list2cmdline(['make', id]))
 
         for file in glob.glob("*.in"):
+            ok = True
+
             with open(file) as input:
                 proc = subprocess.Popen(['./' + id], stdin=input, stdout=subprocess.PIPE)
                 lines2 = proc.communicate()[0].decode('utf-8').splitlines()
 
-                expectPath = os.path.splitext(file)[0] + '.out'
-                with open(expectPath) as expect:
-                    for line in difflib.unified_diff(input.read().splitlines(), expect.read().splitlines(), fromfile='file1', tofile='file2', lineterm=''):
-                        print(line)
+            expectPath = os.path.splitext(file)[0] + '.out'
+            with open(expectPath) as expect:
+                for line in difflib.unified_diff(lines2, expect.read().splitlines(), fromfile='file1', tofile='file2', lineterm=''):
+                    print(line)
+                    ok = False
+
+            if ok:
+                logging.info("test %s is ok" % file)
+            else:
+                logging.error("test %s is failing" % file)
