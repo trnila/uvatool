a65b2b27bd40d8792e0d0ac57b4f137a6edc71c8
Daniel Trnka <daniel.trnka@gmail.com>
2016-10-23 22:38:13 +0200

    cleaned file

--- uva.py
+++ uva.py
@@ -19,20 +19,19 @@ except:
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(help='sub-command help', dest="action")
-parser_a = subparsers.add_parser('init')
-parser_a.add_argument('id', help='id of uva problem')
-parser_a.add_argument('-t', '--template', help='template', default='c')
+sub = subparsers.add_parser('init')
+sub.add_argument('id', help='id of uva problem')
+sub.add_argument('-t', '--template', help='template', default='c')
 
-parser_b = subparsers.add_parser('read')
+subparsers.add_parser('read')
+subparsers.add_parser('inputs')
 
-parser_b = subparsers.add_parser('inputs')
-parser_b = subparsers.add_parser('test')
-parser_b.add_argument('-i', '--input', action='append')
+sub = subparsers.add_parser('test')
+sub.add_argument('-i', '--input', action='append')
 
-parser_b = subparsers.add_parser('generate')
-parser_b.add_argument('input', help='input file')
+sub = subparsers.add_parser('generate')
+sub.add_argument('input', help='input file')
 
-# parse some argument lists
 args = parser.parse_args()
 
 cmd = getattr(command, args.action[0].upper() + args.action[1:])()
